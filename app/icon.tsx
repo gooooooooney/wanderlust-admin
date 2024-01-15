6ce3b6a5522ac37bb39cf1ff172d8b1ae89dcbc5
张文光 <wenguangzhang@caixin.com>
2024-01-15 18:05:30 +0800

    gmail

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,30 +1,30 @@
 import { ImageResponse } from 'next/og'
- 
- 
+
+
 // Image metadata
 export const size = {
   width: 32,
   height: 32,
 }
 export const contentType = 'image/png'
- 
+
 // Image generation
 export default function Icon() {
   return new ImageResponse(
     (
       // ImageResponse JSX element
       <div
-      style={{
-        fontSize: 20,
-        background: 'orange',
-        borderRadius: '50%',
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: 'white',
-      }}
+        style={{
+          fontSize: 20,
+          background: 'orange',
+          borderRadius: '50%',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+        }}
       >
         W
       </div>
--- lib/email.ts
+++ lib/email.ts
@@ -9,9 +9,9 @@ type EmailPayload = {
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
-  // host: process.env.MAIL_HOST,
-  // port: parseInt(process.env.MAIL_PORT || "587"),
-  // secure: false,
+  host: process.env.MAIL_HOST,
+  port: parseInt(process.env.MAIL_PORT || "587"),
+  secure: false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PWD,
